bd550e5007454d8bf92b97b47624ee8c53b67ed9
Nick Mai <nick.mai@outlook.com>
2015-12-09 00:23:24 -0800

    highly advanced algo + weak testing

--- sort.js
+++ sort.js
@@ -1,5 +1,40 @@
-/* (•~•)
+/* sort.js: Sorts an array of strings based on arbitrary lex. ordering.
+ * note- Please ensure you are running this in an ES6 spec-compliant env.
  *
- * Nick Mai 2015
- *
+ * An Exercise in Intellect and Ingenuity™
  */
+
+'use strict'
+
+let sort = function (input, order) {
+
+  // Turn order inside-out (char becomes key, index+1 becomes val).
+  // This is what we will use to compute lexical order later.
+  let points = []
+  order.split('').map((chr, i) => {
+    points[chr] = i + 1
+  })
+
+  // @todo: convert this to factorial of largest point
+  let max = 0
+  points.map((x) => {
+    max += x
+  })
+
+  function compute (str) {
+    let weight = 0
+    str.split("").map((chr, i) => {
+      weight += (points[chr] + 1) * i //@todo: this is wrong, continue here
+    })
+    return weight
+  }
+
+  // obligatory one-liner
+  let weights = input.map((str, i) => { return compute (s) })
+
+  // sigh
+  // fastest is to calculate the position based off max points
+  // but i dunno. fast... not really clean though IMO.
+}
+
+module.exports = sort
---
+++ tests.js
@@ -0,0 +1,10 @@
+// Super primitive testing but it will work for now.
+// Jest/Mocha will come tomorrow morning.
+
+'use strict'
+
+let lex_sort = require('./sort.js')
+
+sort(['abc','cba','bca'], 'abc')
+
+console.log('All tests complete.')
